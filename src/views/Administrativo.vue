77e01bff2f27e2ec48897a4c8e729696c85499b1
pubdaniel <pubdaniel@gmail.com>
2019-11-12 23:47:48 -0300

    melhorada tela de resultados de pesquisa e de queries

--- src/views/Administrativo.vue
+++ src/views/Administrativo.vue
@@ -2,41 +2,24 @@
   <b-container>
     <h1>Histórico de pesquisas</h1>
 
-    <b-table :items="queries" small> </b-table>
+    <b-table :items="queries" :fields="fields" small> </b-table>
   </b-container>
 </template>
 
 <script>
+import Axios from "axios";
+
+
 export default {
+  async mounted() {
+    const response = await Axios.get("http://localhost:8080/PlnTCS/api/query/all?key=1335809234");
+    console.log(response);
+    this.queries = response.data;
+  },
   data() {
     return {
-      queries: [
-        {
-          id: 1,
-          query: "busca sobre Pedro e Maria em Florianópolis",
-          date: "01/01/01 12:00:00",
-          results: 21,
-          entitties: [
-            {
-              person: ["Pedro", "Maria"],
-              location: ["FLorianópolis"],
-            },
-          ],
-          accuracy: "25%",
-        },
-        {
-          id: 2,
-          query: "posts de 12 de maio de joão",
-          date: "12/05/2019",
-          results: 21,
-          entitties: [
-            {
-              person: ["João"],
-            },
-          ],
-          accuracy: "65%",
-        },
-      ],
+      fields: ['message', 'date', 'relevance'],
+      queries: [],
     };
   },
 };
--- src/views/Pesquisa.vue
+++ src/views/Pesquisa.vue
@@ -1,10 +1,14 @@
 <template>
   <b-container>
-    <h1>Pesquisa {{ postId }}</h1>
+    <h1>Pesquisa</h1>
 
     <b-input-group>
-      <b-form-input placeholder="Digite os termos de pesquisa"> </b-form-input>
-      <b-button variant="outline-success">buscar</b-button>
+      <b-form-input
+        placeholder="Digite os termos de pesquisa"
+        v-model="message"
+      >
+      </b-form-input>
+      <b-button variant="outline-success" @click="doSearch()">buscar</b-button>
     </b-input-group>
 
     <p>{{ posts.length }} resultados encontrados</p>
@@ -14,32 +18,62 @@
         v-for="post in posts"
         v-bind:key="post.id"
         button
-        @click="openModal(post.id)"
+        @click="openModal(post)"
       >
-         @{{ post.username}} - {{ post.username }} <br>
-         {{post.text}}
+        <b-row>
+          <b-col cols="10">
+            @{{ post.username }} - {{ post.name }} - Followers:
+            {{ post.followers }} <br />
+            {{ post.text }} <br />
+            {{ post.date }}
+          </b-col>
+          <b-col cols="2">
+            {{ post.relevance === 0 ? "" : post.relevance  }}
+          </b-col>
+        </b-row>
       </b-list-group-item>
     </b-list-group>
 
     <b-modal
       id="post-info-modal"
       content-class="shadow"
-      title="Detalhes de postagem"
+      :title="`Detalhes de postagem id: ${postSelected.id}`"
     >
-      <p class="my-2">Post id: {{ postId }}</p>
+      {{ postSelected.text }}
+
+      <highlightable-input
+        highlight-style="background-color:yellow"
+        :highlight-enabled="true"
+        :highlight="highlight"
+        v-model="postSelected.text"
+      />
     </b-modal>
   </b-container>
 </template>
 
 <script>
 import Axios from "axios";
+import HighlightableInput from "vue-highlightable-input";
 
 export default {
+  components: {
+    HighlightableInput
+  },
   data() {
     return {
+      message: "Carlos peruca Rio 2019",
       currentPage: 0,
+      postSelected: {},
       postId: {},
-      posts: []
+      posts: [],
+      highlight: [
+        { text: "Presidente", style: "background-color:#f37373" },
+        { text: "enfrenta", style: "background-color:#fca88f" },
+        { text: "senhor", style: "background-color:#bbe4cb" },
+        { text: "compreendo", style: "background-color:#fff05e" },
+        "whatever",
+        { start: 2, end: 5, style: "background-color:#f330ff" }
+      ]
     };
   },
   computed: {
@@ -48,13 +82,23 @@ export default {
     }
   },
   methods: {
-    openModal(id) {
-      this.postId = id;
+    openModal(post) {
+      this.postSelected = post;
       this.$bvModal.show("post-info-modal");
+    },
+    async doSearch() {
+      console.log(this.message);
+      const response = await Axios.get(
+        `http://localhost:8080/PlnTCS/api/posts?key=1335809234&text=${this.message}`
+      );
+      console.log(response);
+      this.posts = response.data;
     }
   },
   async mounted() {
-    const response = await Axios.get("http://localhost:8080/PlnTCS/api/posts?key=1575631198790");
+    const response = await Axios.get(
+      "http://localhost:8080/PlnTCS/api/posts?key=1335809234"
+    );
     console.log(response);
     this.posts = response.data;
   }
